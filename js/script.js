6ea82f4978a9b24cef11700f1c6bf9c418867fd8
sujeong01 <95603837+sujeong01@users.noreply.github.com>
2021-12-06 15:17:02 +0900

    두번째업데이트

---
+++ js/script.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function(){
+  $('.navi > li').mouseover(function(){
+    $(this).find('.submenu').stop().slideDown(500);
+  }).mouseout(function(){
+    $(this).find('.submenu').stop.slideUp(500);
+  });
+  $('.imgslide a:gt(0)').hide();
+  setinterval(function(){
+    $('.imgslide a:first-child').fadeOut()
+    .next('a'),fadeIn()
+    .end().appendTo('.imgslide');},3000);
+});
